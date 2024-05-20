71883930375b9fd8a62a912b46ed7a0e5841ae22
JuGabii <167762359+JuGabii@users.noreply.github.com>
2024-05-20 10:50:10 +0000

    wi

---
+++ gameWeb.js
@@ -0,0 +1,17 @@
+// Código no arquivo gamesWeb.js
+
+function colidiu() {
+
+    var largura nave.width;
+    
+    var altura nave.height;
+    
+    return (xNave + largura) > xInimigo &&
+    
+    xNave < (xInimigo + largura) &&
+    
+    (yNave + altura) > ylnimigo &δι
+    
+    yNave (yInimigo + altura)
+    
+    }
